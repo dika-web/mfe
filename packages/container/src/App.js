b7985b1efdd26274252056f1f2e634159f066682
dika-web <dina.katerenuyk@gmail.com>
2025-03-26 17:51:57 +0100

    fix style issue

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,15 +3,25 @@ import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 
+import { StylesProvider , createGenerateClassName} from "@material-ui/core/styles";
+
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co'
+})
+
+
 
 export default () => {
     return  (
-    <BrowserRouter>
-        <div>
-            < Header />
-            < MarketingApp />
-        </div>
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+        <BrowserRouter>
+            <div>
+                < Header />
+                < MarketingApp />
+            </div>
+        </BrowserRouter>
+    </StylesProvider>
     )
 }
 
