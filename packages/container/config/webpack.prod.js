01aab178590d16e6390cd609d2fa0932be1a04d6
dika-web <dina.katerenuyk@gmail.com>
2025-03-26 16:53:59 +0100

    update container yml

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -4,27 +4,27 @@ const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 const domain = process.env.PRODUCTION_DOMAIN || 'https://dvqd9jqo4ull0.cloudfront.net'
 
-module.exports = async () => {
-    const remotes = await getRemotes();
 
-    const prodConfig = {
-      mode: 'production',
-      output: {
-        filename: '[name].[contenthash].js',
-        publicPath: '/container/latest/',
-      },
-      plugins: [
-        new ModuleFederationPlugin({
-          name: 'container',
-          remotes : {
-            marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
-          },
-          shared: packageJson.dependencies,
-        }),
-      ],
-    };
-  
-    return merge(commonConfig, prodConfig);
+const prodConfig = {
+    mode: 'production',
+    output: {
+      filename: '[name].[contenthash].js',
+      publicPath: '/container/latest/',
+    },
+    plugins: [
+      new ModuleFederationPlugin({
+        name: 'container',
+        remotes : {
+          marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
+        },
+        shared: packageJson.dependencies,
+      }),
+    ],
   };
+
+
+
+module.exports = merge(commonConfig, prodConfig);
+
 
 
