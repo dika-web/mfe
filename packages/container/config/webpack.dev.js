3e352b1f613a8d68f5ee97768c61d9c73db717df
dika-web <dina.katerenuyk@gmail.com>
2025-03-26 11:48:19 +0100

    test

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,6 +1,6 @@
-const { merge } = require('webpack-merge')
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
-const commonConfig = require('./webpack.common')
+const { merge } = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
 
 const devConfig = {
     mode: 'development',
