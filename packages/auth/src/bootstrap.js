4976120f9c790a84bef41d605d146255bb7d756c
dika-web <dina.katerenuyk@gmail.com>
2025-03-27 13:04:59 +0100

    add auth sub app

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -3,22 +3,25 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { createMemoryHistory, createBrowserHistory } from 'history';
 
-const mount = (el, { onNavigate , defaultHistory}) => {
-    const history = defaultHistory || createMemoryHistory()
+const mount = (el, { onSignIn, onNavigate , defaultHistory, initialPath}) => {
+    const history = defaultHistory || createMemoryHistory({
+        initialEntries: [initialPath]
+    });
+
 
     if(onNavigate) {
          history.listen(onNavigate)
     }
 
      ReactDOM.render(
-        <App history={history}/>, el
+        <App onSignIn = {onSignIn} history={history}/>, el
     )
 
     return  {
         onParentNavigate({pathname: nextPathName}) {
-            const {pathname} = history.location
+            const {pathname} = history.location;
 
-            if(pathname !== nextPathName) {
+            if (pathname !== nextPathName) {
                 history.push(nextPathName)
             }
         }
@@ -26,7 +29,7 @@ const mount = (el, { onNavigate , defaultHistory}) => {
 }
 
 if (process.env.NODE_ENV === "development") {
-    const devRoot = document.querySelector('#_marketing-dev-root')
+    const devRoot = document.querySelector('#_auth-dev-root')
    
     if(devRoot) {
         mount(devRoot, { defaultHistory: createBrowserHistory()})
--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,9 +1,15 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import MarketingApp from "./components/MarketingApp";
+import React , {lazy, Suspense} from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from "./components/Header";
+import Progress from "./components/Progress";
+
 
 import { StylesProvider , createGenerateClassName} from "@material-ui/core/styles";
+import { useState } from "react";
+
+
+const MarketingLazy = lazy(()=> import('./components/MarketingApp'))
+const AuthLazy = lazy(()=> import('./components/AuthApp'))
 
 
 const generateClassName = createGenerateClassName({
@@ -11,14 +17,21 @@ const generateClassName = createGenerateClassName({
 })
 
 
-
 export default () => {
+    const  [ isSignedIn, setIsSignin ] = useState(false);
     return  (
     <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
             <div>
-                < Header />
-                < MarketingApp />
+                < Header onSignOut={()=> setIsSignin(false)} isSignedIn={isSignedIn} />
+                <Suspense fallback={<Progress/>}>
+                    <Switch>
+                        <Route path ="/auth">
+                             <AuthLazy onSignIn ={()=> setIsSignin(true)}/>
+                        </Route>
+                        <Route path ="/" component= {MarketingLazy}/>
+                    </Switch>
+                </Suspense>
             </div>
         </BrowserRouter>
     </StylesProvider>
